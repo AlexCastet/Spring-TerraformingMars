fbd01df323c0b8ec83e0d6d644f0a23fdee9c5c0
Pchaozz <polsh2001.1@gmail.com>
2023-12-17 22:07:59 +0100

    Vuelto a crear

---
+++ TerraformingMars/src/main/java/Terraformingmars/TerraformingMarsApplication.java
@@ -0,0 +1,13 @@
+package Terraformingmars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraformingMarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraformingMarsApplication.class, args);
+	}
+
+}
---
+++ TerraformingMars/src/test/java/terraformingMars/TerraformingMarsApplicationTests.java
@@ -0,0 +1,13 @@
+package terraformingMars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraformingMarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
